0fa307baaa66fa8c305c5b492bfe73382602f75b
kovrus <ruslan@crate.io>
2016-03-08 17:12:44 +0100

    adapt create start script to recent changes

--- setup/crate/start_crate.sh
+++ setup/crate/start_crate.sh
@@ -41,34 +41,34 @@ while [[ $# > 1 ]]; do
   shift
 done
 
-CRATE_CONTAINERS=($(docker ps -f "name=swarm-agent" --format "{{.Names}}" | cut -d/ -f1 | cut -d- -f3 | sort -g))
+CRATE_CONTAINERS=($(docker ps -a | grep -o "swarm-node-.*" | cut -d/ -f1 | cut -d- -f3 | sort -g))
 NEW_CONTAINERS=()
+LAST=0
 
 if [ ${#CRATE_CONTAINERS[@]} -ne 0 ]; then
-    LAST=${CRATE_CONTAINERS[${#CRATE_CONTAINERS[@]}-1]}
-    FIRST=${CRATE_CONTAINERS[0]}
-    for i in $(eval echo "{$FIRST..$LAST}"); do
-        if [[ ! ${CRATE_CONTAINERS[*]} =~ $i ]]; then
-            echo "swarm-agent-$i";
-        NEW_CONTAINERS+=(swarm-agent-$i)
-        fi
-    done
+   LAST=${CRATE_CONTAINERS[${#CRATE_CONTAINERS[@]}-1]}
+   FIRST=${CRATE_CONTAINERS[0]}
+   for i in $(eval echo "{$FIRST..$LAST}"); do
+       if [[ ! ${CRATE_CONTAINERS[*]} =~ $i ]]; then
+           echo "swarm-node-$i";
+       NEW_CONTAINERS+=(swarm-node-$i)
+       fi
+   done
+   LAST=$(($LAST + 1))
 fi
 
-ADD=$(($INSTANCES + ${#NEW_CONTAINERS[@]}))
-for i in $(eval echo "{$(($LAST + 1))..$(($LAST + $ADD))}"); do
-    NEW_CONTAINERS+=(swarm-agent-$i)
+ADD=$(($INSTANCES + ${#NEW_CONTAINERS[@]} - 1))
+for i in $(eval echo "{$(($LAST))..$(($LAST + $ADD))}"); do
+    NEW_CONTAINERS+=(swarm-node-$i)
 done
 
 for i in "${NEW_CONTAINERS[@]}"; do
     NODE_NAME=$i
-    docker run -d -p 4200:4200 -p 4300:4300 \
-      --name $NODE_NAME \
-      --env="constraint:node==$NODE_NAME"
-      --env="CRATE_HEAP_SIZE=14g"  \
-      --volume /mnt/data1/data:/data1 \
-      crate/crate:a1k \
-      crate  \
+    docker run -d -p 4200:4200 -p 4300:4300 --env="constraint:node==$NODE_NAME" --env="CRATE_HEAP_SIZE=31g" \
+        -v /mnt:/data \
+        --name $NODE_NAME \
+        crate:latest \
+        crate  \
         -Des.cluster.name=crate-swarm \
         -Des.indices.store.throttle.max_bytes_per_sec=200mb \
         -Des.indices.memory.index_buffer_size=15% \
@@ -78,10 +78,10 @@ for i in "${NEW_CONTAINERS[@]}"; do
         -Des.node.data=$ISDATA \
         -Des.http.port=4200 \
         -Des.transport.tcp.port=4300 \
-        -Des.transport.publish_host=''$(hostname -i)''   \
+        -Des.transport.publish_host=$NODE_NAME \
         -Des.multicast.enabled=false  \
         -Des.discovery.zen.minimum_master_nodes=2  \
         -Des.discovery.type=srv  \
         -Des.discovery.srv.query=_azure1k._srv.fir.io \
-        -Des.path.data=/data1
+        -Des.path.data=/data
 done
