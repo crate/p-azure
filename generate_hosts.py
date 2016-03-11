28e20671f3b841008ea4547f9bd1f28a11ca19f7
Christian Haudum <christian.haudum@gmail.com>
2016-03-11 16:00:08 +0100

    fixup! wip

--- generate_hosts.py
+++ generate_hosts.py
@@ -4,4 +4,4 @@
 # Usage:
 # echo $(python generate_hosts.py 3 16)
 import sys
-print(','.join(['swarm-node-{}'.format(x) for x in range(int(sys.argv[1]), int(sys.argv[2])+1)]))
+print(' '.join(['swarm-node-{}'.format(x) for x in range(int(sys.argv[1]), int(sys.argv[2])+1)]))
--- start_loadtest.sh
+++ start_loadtest.sh
@@ -1,10 +1,11 @@
 #/bin/bash
 
-CORES=4
-THREADS=16
-ITERATIONS=100
+CORES=32
+THREADS=32
+ITERATIONS=50
 HOSTS="$1"
+BULK_SIZE=5000
 
 for x in $(seq 1 $CORES); do
-  python3 loadtest.py --iterations $ITERATIONS --threads $THREADS --hosts $HOSTS &
+  python3 loadtest.py --iterations $ITERATIONS --threads $THREADS --bulk-size $BULK_SIZE --hosts $HOSTS &
 done
